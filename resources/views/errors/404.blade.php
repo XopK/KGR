a35d7d9902a433b4f7753f840415a5346174ee31
XopK <dmahmutov12@gmail.com>
2024-11-19 19:41:14 +0500

    Initial commit

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Практикум по КГР</title>
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png"/>
+
+    <!-- PLUGINS CSS STYLE -->
+    <link rel="stylesheet" href="/plugins/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="/plugins/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="/plugins/slick/slick.css">
+    <link rel="stylesheet" href="/plugins/slick/slick-theme.css">
+    <link rel="stylesheet" href="/plugins/fancybox/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="/plugins/aos/aos.css">
+
+    <!-- CUSTOM CSS -->
+    <link href="/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="body-wrapper" data-spy="scroll" data-target=".privacy-nav">
+
+
+<section class="error-page">
+    <div class="center">
+        <div class="block text-center">
+            <h1>404</h1>
+            <h3>Упс! Страница не найдена.</h3>
+            <p>Мы не можем найти страницу, которую вы ищете.
+                <br>Но не переживайте, мы можем помочь вам вернуться на правильный путь!</p>
+            <a href="{{ route('index') }}" class="btn btn-main-md">Вернуться на главную</a>
+        </div>
+    </div>
+</section>
+
+
+<!-- To Top -->
+<div class="scroll-top-to">
+    <i class="ti-angle-up"></i>
+</div>
+
+<!-- JAVASCRIPTS -->
+<script src="/plugins/jquery/jquery.min.js"></script>
+<script src="/plugins/bootstrap/bootstrap.min.js"></script>
+<script src="/plugins/slick/slick.min.js"></script>
+<script src="/plugins/fancybox/jquery.fancybox.min.js"></script>
+<script src="/plugins/syotimer/jquery.syotimer.min.js"></script>
+<script src="/plugins/aos/aos.js"></script>
+
+
+<script src="/js/script.js"></script>
+</body>
+
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,281 @@
+<x-layout>
+    <!--====================================
+    =            Hero Section            =
+    =====================================-->
+    <section class="section gradient-banner">
+        <div class="shapes-container">
+            <div class="shape" data-aos="fade-down-left" data-aos-duration="1500" data-aos-delay="100"></div>
+            <div class="shape" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100"></div>
+            <div class="shape" data-aos="fade-up-right" data-aos-duration="1000" data-aos-delay="200"></div>
+            <div class="shape" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200"></div>
+            <div class="shape" data-aos="fade-down-left" data-aos-duration="1000" data-aos-delay="100"></div>
+            <div class="shape" data-aos="fade-down-left" data-aos-duration="1000" data-aos-delay="100"></div>
+            <div class="shape" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300"></div>
+            <div class="shape" data-aos="fade-down-right" data-aos-duration="500" data-aos-delay="200"></div>
+            <div class="shape" data-aos="fade-down-right" data-aos-duration="500" data-aos-delay="100"></div>
+            <div class="shape" data-aos="zoom-out" data-aos-duration="2000" data-aos-delay="500"></div>
+            <div class="shape" data-aos="fade-up-right" data-aos-duration="500" data-aos-delay="200"></div>
+            <div class="shape" data-aos="fade-down-left" data-aos-duration="500" data-aos-delay="100"></div>
+            <div class="shape" data-aos="fade-up" data-aos-duration="500" data-aos-delay="0"></div>
+            <div class="shape" data-aos="fade-down" data-aos-duration="500" data-aos-delay="0"></div>
+            <div class="shape" data-aos="fade-up-right" data-aos-duration="500" data-aos-delay="100"></div>
+            <div class="shape" data-aos="fade-down-left" data-aos-duration="500" data-aos-delay="0"></div>
+        </div>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 order-2 order-md-1 text-center text-md-left">
+                    <h1 class="text-white font-weight-bold mb-4">Практикум по КГР</h1>
+                    <p class="text-white mb-5">Погрузитесь в увлекательный мир компьютерной графики! <br> Освойте основы
+                        компьютерной графики и откройте для себя мир визуальных технологий.</p>
+                </div>
+                <div class="col-md-6 text-center order-1 order-md-2">
+                    <img class="img-fluid" src="/images/heroImage.png" alt="heroImage">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====  End of Hero Section  ====-->
+
+    <section class="section pt-0 position-relative pull-top">
+        <div class="container">
+            <div class="rounded shadow p-5 bg-white">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 mt-5 mt-md-0 text-center">
+                        <i class="ti-paint-bucket text-primary h1"></i>
+                        <h3 class="mt-4 h5 ">Графика с нуля</h3>
+                        <p class="regular text-muted">Осваивайте ключевые техники создания изображений, от простых форм
+                            до сложных визуальных решений.</p>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mt-5 mt-md-0 text-center">
+                        <i class="ti-light-bulb text-primary h1"></i>
+                        <h3 class="mt-4 h5 ">Творческая лаборатория</h3>
+                        <p class="regular text-muted">Учитесь применять художественные и технические навыки для создания
+                            уникальных проектов.</p>
+                    </div>
+                    <div class="col-lg-4 col-md-12 mt-5 mt-lg-0 text-center">
+                        <i class="ti-layers text-primary h1"></i>
+                        <h3 class="mt-4 h5 ">Графика в действии</h3>
+                        <p class="regular text-muted">Воплощение идей с помощью графических программ: от создания
+                            логотипов до разработки анимаций.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!--==================================
+    =            Feature Grid            =
+    ===================================-->
+    <section class="feature section pt-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 ml-auto justify-content-center">
+                    <!-- Feature Mockup -->
+                    <div class="image-content" data-aos="fade-right">
+                        <img class="img-fluid" style="padding: 15px;"
+                             src="/images/feature/1681203151_kartinki-pibig-info-p-geometricheskie-figuri-kartinki-dlya-preze-12.png"
+                             alt="object">
+                    </div>
+                </div>
+                <div class="col-lg-6 mr-auto align-self-center">
+                    <div class="feature-content">
+                        <!-- Feature Title -->
+                        <h2>Практикум по компьютерной графике</h2>
+                        <!-- Feature Description -->
+                        <p class="desc">Это уникальная возможность изучить основы компьютерной графики и развить свои
+                            навыки в различных ее областях. <br>
+                            На курсе вы освоите не только 3D моделирование, но и
+                            научитесь работать с растровыми и векторными изображениями, а также с анимацией и
+                            графическими эффектами.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="feature section pt-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 ml-auto align-self-center">
+                    <div class="feature-content">
+                        <!-- Feature Title -->
+                        <h2>Практическое обучение и развитие навыков в компьютерной графике</h2>
+                        <!-- Feature Description -->
+                        <p>Занятия будут включать теоретические и практические модули, а также проектные работы, что
+                            позволит вам создать свое портфолио. Вы научитесь работать с современными инструментами и
+                            технологиями, применяя их в дизайне, анимации и цифровом искусстве. Не упустите шанс стать
+                            частью быстро развивающейся индустрии и открыть для себя новые горизонты в мире графики!
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-6 mr-auto justify-content-center">
+                    <!-- Feature mockup -->
+                    <div class="image-content" data-aos="fade-left">
+                        <img class="img-fluid" src="/images/feature/43108-business-team.gif" alt="ipad">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====  End of Feature Grid  ====-->
+
+    <!--=================================
+    =            Video Promo            =
+    ==================================-->
+    <section class="video-promo section bg-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="content-block">
+                        <!-- Heading -->
+                        <h2>Watch Our Promo Video</h2>
+                        <!-- Promotional Speech -->
+                        <p>Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur arcu erat, accumsan id
+                            imperdiet et,
+                            porttitor at sem. Vivamus </p>
+                        <!-- Popup Video -->
+                        <a data-fancybox href="https://www.youtube.com/watch?v=jrkvirglgaQ">
+                            <i class="ti-control-play video"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====  End of Video Promo  ====-->
+
+    <!--=================================
+    =            Testimonial            =
+    ==================================-->
+    <section class="section testimonial" id="testimonial">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- Testimonial Slider -->
+                    <div class="testimonial-slider owl-carousel owl-theme">
+                        <!-- Testimonial 01 -->
+                        <div class="item">
+                            <div class="block shadow">
+                                <!-- Speech -->
+                                <p>
+                                    Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin
+                                    molestie malesuada.
+                                    Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus.
+                                    Lorem ipsum dolor
+                                    sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus.
+                                    Quisque velit nisi,
+                                    pretium ut lacinia in, elementum id enim.
+                                </p>
+                                <!-- Person Thumb -->
+                                <div class="image">
+                                    <img src="images/testimonial/feature-testimonial-thumb.jpg" alt="image">
+                                </div>
+                                <!-- Name and Company -->
+                                <cite>Abraham Linkon , Themefisher.com</cite>
+                            </div>
+                        </div>
+                        <!-- Testimonial 01 -->
+                        <div class="item">
+                            <div class="block shadow">
+                                <!-- Speech -->
+                                <p>
+                                    Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin
+                                    molestie malesuada.
+                                    Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus.
+                                    Lorem ipsum dolor
+                                    sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus.
+                                    Quisque velit nisi,
+                                    pretium ut lacinia in, elementum id enim.
+                                </p>
+                                <!-- Person Thumb -->
+                                <div class="image">
+                                    <img src="images/testimonial/feature-testimonial-thumb.jpg" alt="image">
+                                </div>
+                                <!-- Name and Company -->
+                                <cite>Abraham Linkon , Themefisher.com</cite>
+                            </div>
+                        </div>
+                        <!-- Testimonial 01 -->
+                        <div class="item">
+                            <div class="block shadow">
+                                <!-- Speech -->
+                                <p>
+                                    Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin
+                                    molestie malesuada.
+                                    Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus.
+                                    Lorem ipsum dolor
+                                    sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus.
+                                    Quisque velit nisi,
+                                    pretium ut lacinia in, elementum id enim.
+                                </p>
+                                <!-- Person Thumb -->
+                                <div class="image">
+                                    <img src="images/testimonial/feature-testimonial-thumb.jpg" alt="image">
+                                </div>
+                                <!-- Name and Company -->
+                                <cite>Abraham Linkon , Themefisher.com</cite>
+                            </div>
+                        </div>
+                        <!-- Testimonial 01 -->
+                        <div class="item">
+                            <div class="block shadow">
+                                <!-- Speech -->
+                                <p>
+                                    Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin
+                                    molestie malesuada.
+                                    Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus.
+                                    Lorem ipsum dolor
+                                    sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus.
+                                    Quisque velit nisi,
+                                    pretium ut lacinia in, elementum id enim.
+                                </p>
+                                <!-- Person Thumb -->
+                                <div class="image">
+                                    <img src="images/testimonial/feature-testimonial-thumb.jpg" alt="image">
+                                </div>
+                                <!-- Name and Company -->
+                                <cite>Abraham Linkon , Themefisher.com</cite>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====  End of Testimonial  ====-->
+
+    <section class="call-to-action-app section bg-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>It's time to change your mind</h2>
+                    <p>Download over 2 million humans .Get <a
+                                href="https://themefisher.com/products/small-apps-free-app-landing-page-template/">Small
+                            Apps</a> free forever!
+                        <br>We say you won’t look back.</p>
+                    <ul class="list-inline">
+                        <li class="list-inline-item">
+                            <a href="" class="btn btn-rounded-icon">
+                                <i class="ti-apple"></i>
+                                Iphone
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="" class="btn btn-rounded-icon">
+                                <i class="ti-android"></i>
+                                Android
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="" class="btn btn-rounded-icon">
+                                <i class="ti-microsoft-alt"></i>
+                                Windows
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-layout>
